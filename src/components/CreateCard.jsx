db7c8da6c2a0c9ec1d959c859707dcc5ec5cccc6
manish01101 <manishchotu4@gmail.com>
2024-01-20 01:41:59 +0530

    designed new card template

--- src/components/CreateCard.jsx
+++ src/components/CreateCard.jsx
@@ -1,11 +1,29 @@
 export default function CreateCard() {
-  return <div style={styles.card}>
-    <h1>create a card</h1>
-    <div>
-      <input type="text" placeholder="name"/> <br /><br />
-      <input type="text" placeholder="decription"/>
+  return (
+    <div style={styles.card}>
+      <h1>create a card</h1>
+      <div>
+        <input style={styles.input} type="text" placeholder="name" /> <br />
+        <br />
+        <input style={styles.input} type="text" placeholder="decription" />
+        <br />
+        <br />
+        <div>
+          <div>Interests: </div>
+          <input style={styles.input} type="text" placeholder="enter your interests"/>
+          <button style={styles.sbtn}>Add Interest</button>
+        </div>
+        <br /><br />
+        <div>
+          <div>Add Social Handle: </div>
+          <input style={styles.input} type="text" placeholder="enter platform name" />
+          <br /><br />
+          <input style={styles.input} type="text" placeholder="enter platform name" />
+          <button style={styles.sbtn}>Add Handle</button>
+        </div>
+      </div>
     </div>
-  </div>
+  );
 }
 
 const styles = {
@@ -18,4 +36,33 @@ const styles = {
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
     backgroundColor: "#f8f9fa",
   },
-}
+  btn: {
+    textDecoration: "none",
+    color: "#66f",
+    padding: "7px 9px",
+    backgroundColor: "#fff",
+    display: "inline-block",
+    margin: "8px",
+    border: 'solid',
+    borderRadius: '5px',
+    fontSize: '15px'
+  },
+  input: {
+    padding: '0 6px',
+    width: '200px',
+    lineHeight: '30px',
+    border: '2px solid #66f', 
+    borderRadius: '5px',
+  },
+  sbtn: {
+    backgroundColor: '#66f',
+    textDecoration: "none",
+    color: "#fff",
+    padding: "7px 11px",
+    display: "inline-block",
+    margin: "8px",
+    border: 'solid',
+    borderRadius: '5px',
+    fontSize: '15px'
+  }
+};
